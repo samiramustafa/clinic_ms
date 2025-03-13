2beb63b52d4ece5838d0186d8a2df57121302d46
samiramustafa <samiramustafa138@gmail.com>
2025-03-13 09:43:39 +0200

    create models

--- clinic/admin.py
+++ clinic/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import User,Patient ,Doctor ,Feedback, Appointment,AvailableTime
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Patient)
+admin.site.register(Doctor)
+admin.site.register(Feedback)
+admin.site.register(Appointment)
+admin.site.register(AvailableTime)
