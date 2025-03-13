f45a7908418c553e2693e8c779a488138b03ccd2
samiramustafa <samiramustafa138@gmail.com>
2025-03-13 07:53:05 +0200

    create clinic app

--- clinic/apps.py
+++ clinic/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HospitalConfig(AppConfig):
+class ClinicConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hospital'
+    name = 'clinic'
