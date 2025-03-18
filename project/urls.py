ef1d89251fc9326ec3ed7caa4aa52347f9301c79
samiramustafa <samiramustafa138@gmail.com>
2025-03-18 22:43:08 +0200

    edit venv

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from clinic.views import UserViewSet, PatientViewSet, DoctorViewSet
+from django.contrib import admin
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -8,5 +9,6 @@ router.register(r'patients', PatientViewSet)
 router.register(r'doctors', DoctorViewSet)
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path('', include(router.urls)),
 ]
