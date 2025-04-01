7b005567ece80c98c31c65ee8c00d5dad72febbb
nasser <drnasser.khairy@gmail.com>
2025-04-01 03:53:59 +0200

    pull

--- clinic/serializers.py
+++ clinic/serializers.py
@@ -1,27 +1,5 @@
-# from rest_framework import serializers
-# from .models import User, Patient, Doctor
 
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = '__all__'
-
-# class PatientSerializer(serializers.ModelSerializer):
-#     user = UserSerializer()
-
-#     class Meta:
-#         model = Patient
-#         fields = '__all__'
-
-# class DoctorSerializer(serializers.ModelSerializer):
-#     user = UserSerializer()
-
-#     class Meta:
-#         model = Doctor
-#         fields = '__all__'
 from rest_framework import serializers
-from .models import User, Patient, Doctor
-
 from rest_framework import serializers
 from .models import *
 from django.contrib.auth.hashers import make_password
--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
 from rest_framework.routers import DefaultRouter
-
 from django.contrib import admin
 from clinic.views import AreaListView
 
